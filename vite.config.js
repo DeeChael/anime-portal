2dc8133584bd7a260314ff713afda5279cc57df0
deechael <DeeChael@outlook.com>
2025-12-07 19:14:05 +0800

    fix: rollback to original

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-export default defineConfig(({ mode }) => {
-  const isProduction = mode === 'production';
-  return {
-    base: isProduction ? '/anime-portal/' : '/',
-    plugins: [
-      vue(),
-      vueDevTools(),
-    ],
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
-      },
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/anime-portal/',
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
-  }
+  },
 })
